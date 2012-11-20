6a737fb869005aa5d88f3614214d4a2ccc439cd2
Rune Kobberø-Hansen <rkh@1508.dk>
2012-11-20 15:42:58 +0100

    Added a margin 0 and margin + to cleargrid. Grid color default added. Vars created for cleargrid.

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,43 +1,76 @@
 (function($){ 
 
-	//Documen ready
+	//Define your markup section and grid
+	var cleargridSection = '.section';
+	var cleargridGrid = '.grid';
+
+	//Document ready
 	$(function(){ 
-		
+
+
 	});
 
 	//Window resize
 	$(window).resize(function () {
-		cleargrid();
+
+		cleargrid(cleargridSection, cleargridGrid);
+
 	});
   
 	//Window load
 	$(window).load(function (){
-    	cleargrid();
+
+    	cleargrid(cleargridSection, cleargridGrid);
+
 	});	
+
+
 
 	//****************************************************************************************************************
     //Functions
 	//****************************************************************************************************************
 	
-	//Clear grid
-	function cleargrid(){
-		$('.cleargrid').remove();
-		var section = $('.section');
+	//Cleargrid (courtesy of Johan Rough )
+	function cleargrid(section, grid){
+
+		$('.cleargrid').remove();	
+
 		$(section).each(function() {
+
 		    var rowPixels = 0;
-		    $(this).children('.grid').each(function () {
 
-		        var gridWidth = $(this).width();
-		        rowPixels += gridWidth;
+		    $(this).children(grid).each(function () {
 
-		        if (rowPixels >= $(section).width() - 1) { //I use - 1 because new and old browsers use different dec numbers
+		    	var gridWidth = $(this).width(); //Width used when '.grid' has no margin
+		        var gridWidthOuter = $(this).outerWidth(true);
 
-		            if($(this).parent().is('ul')){
-						$(this).after('<li class="clear cleargrid"></li>');
-					}else{
-						$(this).after('<div class="clear cleargrid"></div>');
-					}	
-		            rowPixels = 0;
+		        if(gridWidth === gridWidthOuter){ // If margin is 0
+
+		        	rowPixels += gridWidth;
+
+			        if (rowPixels >= $(section).width() - 1) { //I use - 1 because new and old browsers use different dec numbers
+			        	
+			            if($(this).parent().is('ul')){
+							$(this).after('<li class="clear cleargrid"></li>');
+						}else{
+							$(this).after('<div class="clear cleargrid"></div>');
+						}	
+			            rowPixels = 0;
+			        }
+
+		        }else{ //If margin is used in the grid.
+
+			        rowPixels += gridWidthOuter;
+
+			        if (rowPixels >= $(section).width() - 1) { //I use - 1 because new and old browsers use different dec numbers
+
+			            if($(this).parent().is('ul')){
+							$(this).after('<li class="clear cleargrid"></li>');
+						}else{
+							$(this).after('<div class="clear cleargrid"></div>');
+						}	
+			            rowPixels = 0;
+			        }
 		        }
 		    });
 		}); 		
@@ -45,3 +78,4 @@
 	
 	
 })( jQuery );
+
