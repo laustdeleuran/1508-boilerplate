40d1df464726091b34d2498d7ddebebc5263fef5
Johan roug <johanroug@gmail.com>
2012-11-26 09:19:33 +0100

    Added input focus javascript

--- resources/js/script.js
+++ resources/js/script.js
@@ -12,16 +12,13 @@
 
 	//Window resize
 	$(window).resize(function () {
-
 		cleargrid(cleargridSection, cleargridGrid);
-
 	});
   
 	//Window load
 	$(window).load(function (){
-
     	cleargrid(cleargridSection, cleargridGrid);
-
+		inputFocus();
 	});	
 
 
@@ -30,6 +27,24 @@
     //Functions
 	//****************************************************************************************************************
 	
+	//Input focus. Used to clear a input on focus.
+	//Just add the class "input" to the input. e.g. : <input class="input" type="text" value="indtast søgeord">
+	function inputFocus(){
+		 $('.input').on('focus', function() {
+	        // On first focus, check to see if we have the default text saved
+	        // If not, save current value to data()
+	        if (!$(this).data('defaultText')) $(this).data('defaultText', $(this).val());
+
+	        // check to see if the input currently equals the default before clearing it
+	        if ($(this).val()==$(this).data('defaultText')) $(this).val('');
+	    });
+	    $('.input').on('blur', function() {
+	        // on blur, if there is no value, set the defaultText
+	        if ($(this).val()=='') $(this).val($(this).data('defaultText')); 
+	    });
+	}
+
+
 	//Cleargrid (courtesy of Johan Rough )
 	function cleargrid(section, grid){
 
