5f4ff3fc10c520c35f04b39a5ec83247351e3b50
Rune Kobberø-Hansen <rkh@1508.dk>
2013-06-17 10:29:59 +0200

    beautify config.js

--- resources/js/config.js
+++ resources/js/config.js
@@ -1,15 +1,15 @@
 requirejs.config({
     baseUrl: '/resources/js/plugins',
     paths: {
-        'main': '../main',
-        'almond': '../libs/almond.0.2.5',
+        'main'              : '../main',
+        'almond'            : '../libs/almond.0.2.5',
         //Libs
-        'jquery': '../libs/jquery-1.9.0.min',
-        'selectivizr':'../libs/selectivizr-min'
+        'jquery'            : '../libs/jquery-1.9.0.min',
+        'selectivizr'       : '../libs/selectivizr-min'
 
     },
     shim: {
-
+        
     },
     deps: ['main', 'selectivizr']
 });
