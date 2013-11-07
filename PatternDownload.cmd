11eb0f2bd5981c2f55f2a0f6c0fb2834f1b27f70
Rune Kobberø-Hansen <rkh@1508.dk>
2013-11-07 09:31:40 +0100

    Removed pattern primer

--- PatternDownload.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-git clone git://github.com/RuneKobberoe/Pattern-Primer.git
-cd Pattern-Primer
-ECHO The Pattern Primer has now been downloaded to: %CD%
-pause
-IF NOT EXIST node_modules\connect CALL npm install connect
-cmd /k StartServer.cmd
