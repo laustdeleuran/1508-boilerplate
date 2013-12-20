851fe1967d1f5daf750c80282ff92093a295498b
Rune <rkh@1508.dk>
2013-12-20 12:13:31 +0100

    batch update

--- StartInterfaceTools.bat
+++ StartInterfaceTools.bat
@@ -11,11 +11,11 @@ set OLDDIR=%CD%
 
 C:
 
-IF exist Ruby200-x64 ( echo SUCCESS: Ruby installed ) ELSE ( echo ERROR: Could not find Ruby directory at C:\Ruby200-x64, please make sure it is installed... continuing. )
+IF exist Ruby200-x64 ( echo SUCCESS: Ruby installed ) ELSE ( echo ERROR: Could not find Ruby directory at C:\Ruby200-x64, please make sure it is installed... continuing.)
 IF exist "Program Files\nodejs" ( echo SUCCESS: Node installed ) ELSE ( echo ERROR: Could not find nodejs directory at C:\Program Files\nodejs, please make sure it is installed... continuing. )
 
 chdir /d %OLDDIR%
 
 IF exist node_modules ( echo SUCCESS: node_modules already installed ) ELSE ( CALL npm install )
-
+pause
 grunt watch
